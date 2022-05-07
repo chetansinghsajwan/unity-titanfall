c3b7c64a1eaeb90e0ca2c075d594d0ee8ed7e877
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2022-05-07 22:24:05 +0530

    Add LogTarget and FileLogTarget

--- Assets/Scripts/GameDebug/FileLogTarget.cs
+++ Assets/Scripts/GameDebug/FileLogTarget.cs
@@ -1,57 +1,27 @@
-using System;
-using UnityEngine;
+using System.IO;
 
 namespace GameLog
 {
-    public class FileLogTarget : ILogTarget
+    public class FileLogTarget : LogTarget
     {
-        public ILogTarget iLogTarget => (ILogTarget)this;
+        public StreamWriter fileSteamWriter { get; private set; }
+        public FileStream fileStream => (FileStream)fileSteamWriter.BaseStream;
 
-        public LogLevel logLevel { get; set; }
-        public LogLevel flushLevel { get; set; }
-
-        public FileLogTarget(string filePath)
+        public FileLogTarget(string filePath, FileMode fileMode = FileMode.CreateNew)
+            : base(LogLevel.INFO, LogLevel.WARN)
         {
+            var fileStream = new FileStream(filePath, fileMode);
+            fileSteamWriter = new StreamWriter(fileStream);
         }
 
-        // Formats the LogMsg and writes
-        public void Log(LogMsg logMsg)
+        protected override void InternalWrite(LogLevel lvl, string msg)
         {
-            if (iLogTarget.CanLog(logMsg) == false)
-                return;
-
-            string fmtMsg = Format(logMsg);
-            Write(logMsg.logLevel, fmtMsg);
+            fileSteamWriter.Write(msg);
         }
 
-        // Writes down to the target, does not format the msg
-        public void Write(LogLevel lvl, string msg)
+        protected override void InternalFlush()
         {
-            // if (lvl == LogLevel.WARN)
-            // {
-            //     Debug.LogWarning(msg);
-            // }
-            // else if (lvl == LogLevel.ERR || lvl == LogLevel.CRITICAL)
-            // {
-            //     Debug.LogError(msg);
-            // }
-            // else
-            // {
-            //     Debug.Log(msg);
-            // }
-        }
-
-        public void Flush(LogLevel lvl)
-        {
-        }
-
-        public void ForceFlush()
-        {
-        }
-
-        protected string Format(LogMsg logMsg)
-        {
-            return String.Format("{0} [{1}]: {2}", logMsg.loggerName, logMsg.logLevel, logMsg.msg);
+            fileSteamWriter.Flush();
         }
     }
 }
