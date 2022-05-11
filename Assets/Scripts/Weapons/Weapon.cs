72561d71f3ecc4d071062fcd0f13cbf86dfaf83c
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2022-05-11 22:10:33 +0530

    Created WeaponSystem

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,110 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(WeaponInputs))]
+public class Weapon : MonoBehaviour
+{
+    //////////////////////////////////////////////////////////////////
+    /// Variables
+    //////////////////////////////////////////////////////////////////
+
+    public WeaponInputs weaponInputs { get; protected set; }
+    public WeaponBehaviour[] weaponBehaviours { get; protected set; }
+
+    //////////////////////////////////////////////////////////////////
+    /// Updates
+    //////////////////////////////////////////////////////////////////
+
+    void Awake()
+    {
+        weaponInputs = GetComponent<WeaponInputs>();
+        var initializer = GetComponent<WeaponInitializer>();
+
+        CollectBehaviours(weaponInputs);
+        InitBehaviours(initializer);
+    }
+
+    void Update()
+    {
+        UpdateBehaviours();
+    }
+
+    void FixedUpdate()
+    {
+        FixedUpdateBehaviours();
+    }
+
+    void OnDestroy()
+    {
+        DestroyBehaviours();
+    }
+
+    //////////////////////////////////////////////////////////////////
+    /// Behaviours
+    //////////////////////////////////////////////////////////////////
+
+    protected virtual void CollectBehaviours(params WeaponBehaviour[] exceptions)
+    {
+        List<WeaponBehaviour> weaponBehavioursList = new List<WeaponBehaviour>();
+        GetComponents<WeaponBehaviour>(weaponBehavioursList);
+
+        // no need to check for exceptional behaviours
+        if (exceptions.Length > 0)
+        {
+            weaponBehavioursList.RemoveAll((WeaponBehaviour behaviour) =>
+            {
+                foreach (var exceptionBehaviour in exceptions)
+                {
+                    if (behaviour == exceptionBehaviour)
+                        return true;
+                }
+
+                return false;
+            });
+        }
+
+        weaponBehaviours = weaponBehavioursList.ToArray();
+    }
+
+    protected virtual void InitBehaviours(WeaponInitializer initializer)
+    {
+        weaponInputs.OnInitWeapon(this, initializer);
+
+        foreach (var behaviour in weaponBehaviours)
+        {
+            behaviour.OnInitWeapon(this, initializer);
+        }
+    }
+
+    protected virtual void UpdateBehaviours()
+    {
+        weaponInputs.OnUpdateWeapon();
+
+        foreach (var behaviour in weaponBehaviours)
+        {
+            behaviour.OnUpdateWeapon();
+        }
+    }
+
+    protected virtual void FixedUpdateBehaviours()
+    {
+        weaponInputs.OnFixedUpdateWeapon();
+
+        foreach (var behaviour in weaponBehaviours)
+        {
+            behaviour.OnFixedUpdateWeapon();
+        }
+    }
+
+    protected virtual void DestroyBehaviours()
+    {
+        weaponInputs.OnDestroyWeapon();
+
+        foreach (var behaviour in weaponBehaviours)
+        {
+            behaviour.OnDestroyWeapon();
+        }
+    }
+}
