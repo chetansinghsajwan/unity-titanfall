09fefb7188eb74067806ed31654f18aba66557ea
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2024-04-26 16:24:57 +0530

    refactor: refactor code.

--- Assets/Scripts/Characters/CharacterCapsule.cs
+++ Assets/Scripts/Characters/CharacterCapsule.cs
@@ -1,6 +1,8 @@
 using System;
 using UnityEngine;
 using GameFramework.Extensions;
+using System.Diagnostics.Contracts;
+using UnityEngine.Assertions.Must;
 
 struct CharacterCapsule
 {
@@ -10,40 +12,40 @@ struct CharacterCapsule
 
     public bool CapsuleCast(Vector3 move, out RaycastHit hit)
     {
-        bool result = CapsuleCast(move, out RaycastHit smallHit, out RaycastHit bigHit);
-        hit = smallHit.collider ? smallHit : bigHit;
+        bool result = CapsuleCast(move, out RaycastHit innerHit, out RaycastHit outerHit);
+        hit = innerHit.collider ? innerHit : outerHit;
 
         return result;
     }
 
-    public bool CapsuleCast(Vector3 move, out RaycastHit smallHit, out RaycastHit bigHit)
+    public bool CapsuleCast(Vector3 move, out RaycastHit innerHit, out RaycastHit outerHit)
     {
         if (skinWidth > 0f)
         {
-            VirtualCapsule bigCapsule = capsule;
-            bigCapsule.radius += skinWidth;
-            bigCapsule.CapsuleCast(move, out bigHit);
+            VirtualCapsule outerCapsule = capsule;
+            outerCapsule.radius += skinWidth;
+            outerCapsule.CapsuleCast(move, out outerHit);
 
-            if (bigHit.collider)
+            if (outerHit.collider)
             {
-                move = move.normalized * bigHit.distance;
+                move = move.normalized * outerHit.distance;
             }
 
-            capsule.CapsuleCast(move, out smallHit);
+            capsule.CapsuleCast(move, out innerHit);
         }
         else
         {
-            capsule.CapsuleCast(move, out bigHit);
-            smallHit = bigHit;
+            capsule.CapsuleCast(move, out outerHit);
+            innerHit = outerHit;
         }
 
-        return smallHit.collider || bigHit.collider;
+        return innerHit.collider || outerHit.collider;
     }
 
     public bool BaseSphereCast(Vector3 move, out RaycastHit hit)
     {
-        bool result = BaseSphereCast(move, out RaycastHit smallHit, out RaycastHit bigHit);
-        hit = smallHit.collider ? smallHit : bigHit;
+        bool result = BaseSphereCast(move, out RaycastHit innerHit, out RaycastHit outerHit);
+        hit = innerHit.collider ? innerHit : outerHit;
 
         return result;
     }
@@ -56,75 +58,75 @@ struct CharacterCapsule
         return didHit;
     }
 
-    public bool BaseSphereCast(Vector3 move, out RaycastHit smallHit, out RaycastHit bigHit)
+    public bool BaseSphereCast(Vector3 move, out RaycastHit innerHit, out RaycastHit outerHit)
     {
         if (skinWidth > 0f)
         {
-            VirtualCapsule bigCapsule = capsule;
-            bigCapsule.radius += skinWidth;
-            bigCapsule.BaseSphereCast(move, out bigHit);
+            VirtualCapsule outerCapsule = capsule;
+            outerCapsule.radius += skinWidth;
+            outerCapsule.BaseSphereCast(move, out outerHit);
 
-            if (bigHit.collider)
+            if (outerHit.collider)
             {
-                move = move.normalized * bigHit.distance;
+                move = move.normalized * outerHit.distance;
             }
 
-            capsule.BaseSphereCast(move, out smallHit);
+            capsule.BaseSphereCast(move, out innerHit);
         }
         else
         {
-            capsule.BaseSphereCast(move, out bigHit);
-            smallHit = bigHit;
+            capsule.BaseSphereCast(move, out outerHit);
+            innerHit = outerHit;
         }
 
-        return smallHit.collider || bigHit.collider;
+        return innerHit.collider || outerHit.collider;
     }
 
     public Vector3 CapsuleMove(Vector3 move)
     {
-        return CapsuleMove(move, out RaycastHit smallHit, out RaycastHit bigHit);
+        return CapsuleMove(move, out RaycastHit innerHit, out RaycastHit outerHit);
     }
 
     public Vector3 CapsuleMove(Vector3 move, out RaycastHit hit)
     {
-        Vector3 moved = CapsuleMove(move, out RaycastHit smallHit, out RaycastHit bigHit);
-        hit = smallHit.collider ? smallHit : bigHit;
+        Vector3 moved = CapsuleMove(move, out RaycastHit innerHit, out RaycastHit outerHit);
+        hit = innerHit.collider ? innerHit : outerHit;
 
         return moved;
     }
 
     public Vector3 CapsuleMove(Vector3 move, out RaycastHit hit, out Vector3 hitNormal)
     {
-        var moved = CapsuleMove(move, out RaycastHit smallHit, out RaycastHit bigHit, out hitNormal);
-        hit = smallHit.collider ? smallHit : bigHit;
+        var moved = CapsuleMove(move, out RaycastHit innerHit, out RaycastHit outerHit, out hitNormal);
+        hit = innerHit.collider ? innerHit : outerHit;
 
         return moved;
     }
 
-    public Vector3 CapsuleMove(Vector3 move, out RaycastHit smallHit, out RaycastHit bigHit)
+    public Vector3 CapsuleMove(Vector3 move, out RaycastHit innerHit, out RaycastHit outerHit)
     {
         if (move.magnitude == 0f)
         {
-            smallHit = new RaycastHit();
-            bigHit = new RaycastHit();
+            innerHit = new RaycastHit();
+            outerHit = new RaycastHit();
 
             // CapsuleResolvePenetration();
 
             return Vector3.zero;
         }
 
-        CapsuleCast(move, out smallHit, out bigHit);
+        CapsuleCast(move, out innerHit, out outerHit);
 
         Vector3 direction = move.normalized;
         float distance = move.magnitude;
 
-        if (smallHit.collider)
+        if (innerHit.collider)
         {
-            distance = smallHit.distance - skinWidth - COLLISION_OFFSET;
+            distance = innerHit.distance - skinWidth - COLLISION_OFFSET;
         }
-        else if (bigHit.collider)
+        else if (outerHit.collider)
         {
-            distance = bigHit.distance - COLLISION_OFFSET;
+            distance = outerHit.distance - COLLISION_OFFSET;
         }
 
         capsule.position += direction * distance;
@@ -134,10 +136,10 @@ struct CharacterCapsule
         return direction * Math.Max(0f, distance);
     }
 
-    public Vector3 CapsuleMove(Vector3 move, out RaycastHit smallHit, out RaycastHit bigHit, out Vector3 hitNormal)
+    public Vector3 CapsuleMove(Vector3 move, out RaycastHit innerHit, out RaycastHit outerHit, out Vector3 hitNormal)
     {
-        Vector3 moved = CapsuleMove(move, out smallHit, out bigHit);
-        RaycastHit hit = smallHit.collider ? smallHit : bigHit;
+        Vector3 moved = CapsuleMove(move, out innerHit, out outerHit);
+        RaycastHit hit = innerHit.collider != null ? innerHit : outerHit;
 
         RecalculateNormal(hit, move.normalized, out hitNormal);
 
@@ -150,10 +152,10 @@ struct CharacterCapsule
 
         if (skinWidth > 0f)
         {
-            VirtualCapsule bigCapsule = capsule;
-            bigCapsule.radius += skinWidth;
+            VirtualCapsule outerCapsule = capsule;
+            outerCapsule.radius += skinWidth;
 
-            resolve = bigCapsule.ResolvePenetration(collider, COLLISION_OFFSET);
+            resolve = outerCapsule.ResolvePenetration(collider, COLLISION_OFFSET);
         }
         else
         {
@@ -177,38 +179,38 @@ struct CharacterCapsule
 
     public bool RecalculateNormal(RaycastHit hit, Vector3 direction, out Vector3 normal)
     {
-        if (hit.collider && direction != Vector3.zero)
+        Contract.Assert(hit.collider != null);
+
+        if (direction == Vector3.zero)
         {
-            Vector3 origin = hit.point + (-direction * RECALCULATE_NORMAL_FALLBACK);
-            const float rayDistance = RECALCULATE_NORMAL_FALLBACK + RECALCULATE_NORMAL_ADDON;
-
-            Physics.Raycast(origin, direction, out RaycastHit rayHit,
-                rayDistance, capsule.layerMask, capsule.triggerInteraction);
-
-            if (rayHit.collider && rayHit.collider == hit.collider)
-            {
-                normal = rayHit.normal;
-                return true;
-            }
+            normal = hit.normal;
+            return false;
         }
 
-        if (RecalculateNormal(hit, out normal))
+        Vector3 origin = hit.point + (-direction * RECALCULATE_NORMAL_FALLBACK);
+        const float rayDistance = RECALCULATE_NORMAL_FALLBACK + RECALCULATE_NORMAL_ADDON;
+
+        Physics.Raycast(origin, direction, out RaycastHit rayHit,
+            rayDistance, capsule.layerMask, capsule.triggerInteraction);
+
+        if (rayHit.collider == null || rayHit.collider != hit.collider)
         {
-            return true;
+            normal = hit.normal;
+            return false;
         }
 
-        normal = Vector3.zero;
-        return false;
+        normal = rayHit.normal;
+        return true;
     }
 
     public bool RecalculateNormalIfZero(RaycastHit hit, ref Vector3 normal)
     {
-        if (normal == Vector3.zero)
+        if (normal != Vector3.zero)
         {
-            return RecalculateNormal(hit, out normal);
+            return true;
         }
 
-        return true;
+        return RecalculateNormal(hit, out normal);
     }
 
     public VirtualCapsule capsule;
